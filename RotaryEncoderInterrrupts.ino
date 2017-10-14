419a73d5968bbb2ad24d5c5f659c54c16be018fb
Ralph Bacon <arduino@jargongeneration.uk>
2017-10-14 18:37:47 +0100

    The original sketch with the way we detect bounces changed

---
+++ RotaryEncoderInterrrupts.ino
@@ -0,0 +1,88 @@
+// Used for generating interrupts using CLK signal
+const int PinA = 2;
+
+// Used for reading DT signal
+const int PinB = 3;
+
+// Used for the push button switch
+const int PinSW = 8;
+
+// Keep track of last rotary value
+int lastCount = 50;
+
+// Updated by the ISR (Interrupt Service Routine)
+volatile int virtualPosition = 50;
+
+// ------------------------------------------------------------------
+// INTERRUPT     INTERRUPT     INTERRUPT     INTERRUPT     INTERRUPT
+// ------------------------------------------------------------------
+void isr ()  {
+  static unsigned long lastInterruptTime = 0;
+  unsigned long interruptTime = millis();
+
+  // If interrupts come faster than 5ms, assume it's a bounce and ignore
+  if (interruptTime - lastInterruptTime > 5) {
+    if (digitalRead(PinB) == LOW)
+    {
+      virtualPosition-- ; // Could be -5 or -10
+    }
+    else {
+      virtualPosition++ ; // Could be +5 or +10
+    }
+
+    // Restrict value from 0 to +100
+    //virtualPosition = min(100, max(0, virtualPosition));
+
+
+  }
+  // Keep track of when we were here last (no more than every 5ms)
+  lastInterruptTime = interruptTime;
+}
+
+// ------------------------------------------------------------------
+// SETUP    SETUP    SETUP    SETUP    SETUP    SETUP    SETUP
+// ------------------------------------------------------------------
+void setup() {
+  // Just whilst we debug, view output on serial monitor
+  Serial.begin(115200);
+
+  // Rotary pulses are INPUTs
+  pinMode(PinA, INPUT);
+  pinMode(PinB, INPUT);
+
+  // Switch is floating so use the in-built PULLUP so we don't need a resistor
+  pinMode(PinSW, INPUT_PULLUP);
+
+  // Attach the routine to service the interrupts
+  attachInterrupt(digitalPinToInterrupt(PinA), isr, LOW);
+
+  // Ready to go!
+  Serial.println("Start");
+}
+
+// ------------------------------------------------------------------
+// MAIN LOOP     MAIN LOOP     MAIN LOOP     MAIN LOOP     MAIN LOOP
+// ------------------------------------------------------------------
+void loop() {
+
+  // Is someone pressing the rotary switch?
+  if ((!digitalRead(PinSW))) {
+    virtualPosition = 50;
+    while (!digitalRead(PinSW))
+      delay(10);
+    Serial.println("Reset");
+  }
+
+  // If the current rotary switch position has changed then update everything
+  if (virtualPosition != lastCount) {
+
+    // Write out to serial monitor the value and direction
+    Serial.print(virtualPosition > lastCount ? "Up  :" : "Down:");
+    Serial.println(virtualPosition);
+
+    // Keep track of this new value
+    lastCount = virtualPosition ;
+  }
+
+  //delay(100);
+}
